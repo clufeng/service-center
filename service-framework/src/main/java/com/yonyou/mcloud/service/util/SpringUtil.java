c20803476c97df1c41b69b7f34de087e2c064eb9
hubo <hubo02090209@sina.com>
2016-01-14 23:31:44 +0800

    create

---
+++ service-framework/src/main/java/com/yonyou/mcloud/service/util/SpringUtil.java
@@ -0,0 +1,32 @@
+package com.yonyou.mcloud.service.util;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by hubo on 2016/1/7
+ */
+public class SpringUtil {
+
+    private volatile static ApplicationContext context;
+
+    public static Object getBean(String name) {
+        return context.getBean(name);
+    }
+
+    private static ApplicationContext getContext() {
+        if(context == null) {
+            synchronized (SpringUtil.class) {
+                if(context == null) {
+                    context = new ClassPathXmlApplicationContext("applicationContext.xml");
+                    if(context instanceof ConfigurableApplicationContext) {
+                        ((ConfigurableApplicationContext)context).registerShutdownHook();
+                    }
+                }
+            }
+        }
+        return context;
+    }
+
+}
